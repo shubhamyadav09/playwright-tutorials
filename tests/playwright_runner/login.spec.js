7f61e37149382467b7d53b6036e7bf5b50555197
shubham yadav <shubham.y@browserstack.com>
2021-09-10 23:35:01 +0530

    workflow cleanup

--- tests/playwright_runner/login.spec.js
+++ tests/playwright_runner/login.spec.js
@@ -3,5 +3,5 @@ const { test, expect } = require('@playwright/test');
 test('landing page test', async ({ page }) => {
     await page.goto('https://ultimateqa.com/fake-landing-page');
     const title = await page.title();
-    await expect(title).toEqual('Fake landing page - Ultimate QA');
+    await expect(title).toEqual('Fake landing page - Ultimate QA ');
 });
